9ade5efa4390f01540356c679c59e364eaf39b95
bengroneman <bluegroneman10@gmail.com>
2021-11-18 20:18:47 -0700

    oh my bash

--- init.sh
+++ init.sh
@@ -1,12 +1,19 @@
 #bin/bash
 
+# Oh My Bash!
+sh -c "$(curl -fsSL https://raw.github.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+
 # TODO: add some checks to see if it exists already
+# Install NVIM
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 ./nvim.appimage
 rm nvim.appimage
 
+# Install Plug
 # TODO this assumes we are using linux, fix this
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
+mkdir ~/.config
+cp ./2021.vim ~/.config/init.vim
